b6bffc39480c91e38c755d403ef3927d910b4ad0
Worraanong Chanchaijak <wochan@ttu.ee>
2021-10-20 04:41:14 +0300

    [PASS] - ValidateFileExists

--- BPMTool/BPMTool/Program.cs
+++ BPMTool/BPMTool/Program.cs
@@ -7,13 +7,40 @@ namespace BPMTool
     {
         static void Main(string[] args)
         {
-            
+            string path;
+            if (args.Length != 0)
+            {
+                path = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Input a file path:");
+                path = Console.ReadLine();
+            }
+            FileInfo fileInfo = new FileInfo(path);
+            var err = IsFileExists(fileInfo);
+            if (err != null)
+            {
+                ShowError(err);
+            }
         }
 
         public static string IsFileExists(FileInfo fileInfo)
         {
-            throw new NotImplementedException();
+            var isValid = fileInfo.Exists;
+            if (!isValid)
+            {
+                return "ERROR: File not found";
+            }
+            return string.Empty;
         }
 
+        public static void ShowError(string err)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(err);
+            Console.ResetColor();
+        }
     }
+
 }
